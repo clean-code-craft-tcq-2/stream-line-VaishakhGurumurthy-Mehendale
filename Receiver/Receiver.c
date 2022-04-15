9002b5cee21dad056477e44cb880d3ab8396ec48
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-04-15 23:32:47 +0530

    Update Receiver.c

--- Receiver/Receiver.c
+++ Receiver/Receiver.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "Receiver.h"
 
+void ReadConsoleData()
+{
+  unsigned char data;
+  for(int i=0;i<1000;i++)
+  {
+    scanf("%c",data);
+     printf("%c",data);
+  }
+}
