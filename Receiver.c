46eea5752ae084908f11a41efe0cad27b20c3da5
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-04-16 20:56:53 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -4,14 +4,29 @@
 #include "Receiver.h"
 
 char data[1000];
+char Amps_c[5];
+char Kelvin_c[5];
 int Amps[100];
 int kelvin[100];
 int AmpsFlag_Count=0;
 int KelvinFlag_Count=0;
-char Amps_c[5];
-char Kelvin_c[5];
 int AmpsFlag=0;
 int KelvingFlag=0;
+int AIndex_I=0;
+
+int ConvertCharToInt(int AmpsFlag_Count,char *Amps_c)
+{
+  int counter=0;
+  int data=0;
+  while(counter != AmpsFlag_Count)
+  {
+    data=data*10;
+    data+=Amps_c[counter];
+    counter++;
+  }
+  printf("Converted data= %d \n",data);
+  return data;
+}
 
 void FetchAmpsandKelvindata()
 {
@@ -29,6 +44,7 @@ void FetchAmpsandKelvindata()
         KelvingFlag=1;
         AmpsFlag=0;
         printf("A=%s ",Amps_c);
+        Amps[AIndex_I]=ConvertCharToInt(AmpsFlag_Count,Amps_c)
         memset(Amps_c, 0, 5);
         AmpsFlag_Count=0;
       }
